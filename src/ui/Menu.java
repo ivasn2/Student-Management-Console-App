a2429111386baf8343cc1ed716c92a5f23de9756
ivasn2 <ivan.zharov0506@gmail.com>
2026-06-02 21:19:42 +0500

    exception handling added

--- src/ui/Menu.java
+++ src/ui/Menu.java
@@ -26,50 +26,54 @@ public class Menu {
             System.out.println("5. Удалить студента");
             System.out.println("0. Выход");
 
-            // Обработка исключения
-            System.out.print("Выберите действие: ");
-            int choice = scanner.nextInt();
+            try {
 
-            scanner.nextLine();
+                System.out.print("Выберите действие: ");
+                int choice = scanner.nextInt();
+                scanner.nextLine();
 
-            switch (choice) {
+                switch (choice) {
 
-                case 1:
-                    addStudent();
-                    break;
+                    case 1:
+                        addStudent();
+                        break;
 
-                case 2:
-                    showAll();
-                    break;
+                    case 2:
+                        showAll();
+                        break;
 
-                case 3:
-                    findById();
-                    break;
+                    case 3:
+                        findById();
+                        break;
 
-                case 4:
-                    updateStudent();
-                    break;
+                    case 4:
+                        updateStudent();
+                        break;
 
-                case 5:
-                    deleteStudent();
-                    break;
+                    case 5:
+                        deleteStudent();
+                        break;
 
-                case 0:
-                    System.out.println("Выход из программы...");
-                    return;
+                    case 0:
+                        System.out.println("Выход из программы...");
+                        return;
+
+                    default:
+                        System.out.println(
+                                "Такого пункта меню не существует");
+                }
+
+            } catch (InputMismatchException e) {
+
+                System.out.println(
+                        "Номер пункта меню должен быть числом!");
+                scanner.nextLine();
             }
         }
-
-
-
-
-
-
-
     }
 
     // Выводит список всех студентов. Если список пуст, отображает сообщение «Список пуст».
-    public void showAll() {
+    public void showAll () {
 
         List<Student> students = repository.getAll();
 
@@ -84,50 +88,138 @@ public class Menu {
     }
 
     // Запрашивает у пользователя ID студента, выполняет поиск и выводит найденного студента или сообщение «Не найден».
-    public void findById() {
+    public void findById () {
 
-        System.out.print("Введите ID студента, которого хотите найти: ");
-        int choiceId = scanner.nextInt();
+        try {
 
-        if (repository.getById(choiceId) == null) {
-            System.out.println("Не найден");
-            return;
+            System.out.print("Введите ID студента: ");
+            int id = scanner.nextInt();
+
+            Student student = repository.getById(id);
+
+            if (student == null) {
+                System.out.println("Студент не найден");
+                return;
+            }
+
+            System.out.println(student);
+
+        } catch (InputMismatchException e) {
+
+            System.out.println("ID должен быть числом!");
+            scanner.nextLine();
+
         }
-
-        System.out.println(repository.getById(choiceId));
-
     }
 
     // Запрашивает данные нового студента и передает их в метод repository.add() для добавления в хранилище.
-    public void addStudent() {
+    public void addStudent () {
 
         try {
-            System.out.println("Введите имя студента: ");
+
+            System.out.print("Введите имя студента: ");
             String nameStudent = scanner.nextLine();
 
-            System.out.println("Введите возраст: ");
+            // Проверка имени
+            if (!nameStudent.matches("[а-яА-ЯёЁa-zA-Z ]+")) {
+                throw new IllegalArgumentException(
+                        "Имя должно содержать только буквы");
+            }
+
+            System.out.print("Введите возраст: ");
             int ageStudent = scanner.nextInt();
 
-            System.out.println("Введите курс (1-6): ");
+            if (ageStudent <= 0) {
+                throw new IllegalArgumentException(
+                        "Возраст должен быть больше 0");
+            }
+
+            System.out.print("Введите курс (1-6): ");
             int studentCourse = scanner.nextInt();
 
             if (studentCourse < 1 || studentCourse > 6) {
-                throw new IllegalArgumentException("Курс должен быть от 1 до 6");
+                throw new IllegalArgumentException(
+                        "Курс должен быть от 1 до 6");
             }
 
             repository.add(nameStudent, ageStudent, studentCourse);
             System.out.println("Студент успешно добавлен!");
 
         } catch (InputMismatchException e) {
-            System.out.println("Ошибка");
-        }
 
+            System.out.println("Возраст и курс должны быть числами!");
+            scanner.nextLine();
+
+        } catch (IllegalArgumentException e) {
+
+            System.out.println(e.getMessage());
+
+        }
     }
 
     // Запрашивает ID студента и новые данные, затем вызывает repository.update() для обновления информации.
-    public void updateStudent() {
+    public void updateStudent () {
 
         try {
+
+            System.out.print("Введите ID студента: ");
+            int studentId = scanner.nextInt();
+            scanner.nextLine();
+
+            if (repository.getById(studentId) == null) {
+                System.out.println("Студент не найден");
+                return;
+            }
+
+            System.out.print("Введите новое имя: ");
+            String newName = scanner.nextLine();
+
+            if (!newName.matches("[а-яА-ЯёЁa-zA-Z ]+")) {
+                throw new IllegalArgumentException(
+                        "Имя должно содержать только буквы");
+            }
+
+            System.out.print("Введите новый возраст: ");
+            int newAge = scanner.nextInt();
+
+            if (newAge <= 0) {
+                throw new IllegalArgumentException(
+                        "Возраст должен быть больше 0");
+            }
+
+            System.out.print("Введите новый курс (1-6): ");
+            int newCourse = scanner.nextInt();
+
+            if (newCourse < 1 || newCourse > 6) {
+                throw new IllegalArgumentException(
+                        "Курс должен быть от 1 до 6");
+            }
+
+            repository.update(
+                    studentId,
+                    newName,
+                    newAge,
+                    newCourse);
+
+            System.out.println("Данные успешно обновлены");
+
+        } catch (InputMismatchException e) {
+
+            System.out.println("Возраст, курс и ID должны быть числами!");
+            scanner.nextLine();
+
+        } catch (IllegalArgumentException e) {
+
+            System.out.println(e.getMessage());
+
+        }
+    }
+
+    // Запрашивает ID студента и вызывает repository.delete() для удаления записи из хранилища.
+    public void deleteStudent () {
+
+        try {
+
             System.out.print("Введите ID студента: ");
             int studentId = scanner.nextInt();
 
@@ -136,42 +228,15 @@ public class Menu {
                 return;
             }
 
-            System.out.print("Введите новое имя: ");
-            String newStudentName = scanner.nextLine();
+            repository.delete(studentId);
+            System.out.println("Студент удален");
 
-            System.out.print("Введите новый возраст: ");
-            int newStudentAge = scanner.nextInt();
+        } catch (InputMismatchException e) {
 
-            System.out.print("Введите новый курс: ");
-            int newStudentCourse = scanner.nextInt();
+            System.out.println("ID должен быть числом!");
+            scanner.nextLine();
 
-            repository.update(studentId, newStudentName, newStudentAge, newStudentCourse);
-            System.out.println("Данные успешно обновлены");
-
-        } catch (Exception e) {
-            throw new RuntimeException(e);
         }
-
     }
-
-    // Запрашивает ID студента и вызывает repository.delete() для удаления записи из хранилища.
-    public void deleteStudent() {
-
-        System.out.print("Введите ID студента для удаления: ");
-        int studentId = scanner.nextInt();
-
-        if (repository.getById(studentId) == null) {
-            System.out.println("Студент не найден");
-            return;
-        }
-
-        repository.delete(studentId);
-        System.out.println("Студент удален");
-
-    }
-
-
-
-
 
 }
